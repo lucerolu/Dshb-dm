66a3164d137e46608c7acfe94c7564238c50cbde
lucerolu <luceroluna582@gmail.com>
2025-08-18 17:40:59 -0600

    mejor no ando probando cosas

--- dashboard.py
+++ dashboard.py
@@ -336,7 +336,6 @@ if authentication_status:
                 custom_data=["sucursal", "codigo", "abreviatura"]
             )
 
-            # --- Ajustar opacidad de todas las líneas ---
             fig.update_traces(
                 hovertemplate=(
                     "<b>Fecha:</b> %{x}<br>"
@@ -344,14 +343,13 @@ if authentication_status:
                     "<b>Sucursal:</b> %{customdata[0]}<br>"
                     "<b>División:</b> %{customdata[2]}<br>"
                     "<b>Monto:</b> $%{y:,.2f}<extra></extra>"
-                ),
-                line=dict(width=2),
-                opacity=0.3  # líneas atenuadas por defecto
+                )
             )
 
-            # --- Layout para resaltar línea al pasar el mouse ---
             fig.update_layout(
-                hovermode="x",  # resalta línea más cercana al eje x
+                xaxis_title="Fecha de exigibilidad",
+                yaxis_title="Monto",
+                hovermode="closest",  # <-- cambiar aquí
                 template="plotly_white"
             )
 
